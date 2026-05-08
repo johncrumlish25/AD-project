aad52563755e7910202f141311172e776a5fb044
johncrumlish25 <johncrumlish16@gmail.com>
2026-05-08 15:23:08 +0100

    Created config file

---
+++ config.py
@@ -0,0 +1,10 @@
+# MySQL Configuration
+MYSQL_HOST = "localhost"
+MYSQL_USER = "root"
+MYSQL_PASSWORD = ""
+MYSQL_DATABASE = "appdbproj"
+
+# Neo4j Configuration
+NEO4J_URI = "bolt://localhost:7687"
+NEO4J_USER = "neo4j"
+NEO4J_PASSWORD = "root1234"
