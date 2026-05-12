be2f3bc1d21df22dd1f74d3dc825da3be2fdd35f
johncrumlish25 <johncrumlish16@gmail.com>
2026-05-12 14:32:03 +0100

    Edited Neo4j URI

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ MYSQL_PASSWORD = ""
 MYSQL_DATABASE = "appdbproj"
 
 # Neo4j Configuration
-NEO4J_URI = "bolt://localhost:7687"
+NEO4J_URI = "neo4j://127.0.0.1:7687"
 NEO4J_USER = "neo4j"
 NEO4J_PASSWORD = "root1234"
